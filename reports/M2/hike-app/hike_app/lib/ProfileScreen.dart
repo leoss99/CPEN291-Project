4c10f44196e6d4ad30f3584ef4b5c1ab2607b50d
Andrew Forde <awforde@gmail.com>
2021-05-02 18:54:00 -0700

    First implementation of preferences in profile screen

--- reports/M2/hike-app/hike_app/lib/ProfileScreen.dart
+++ reports/M2/hike-app/hike_app/lib/ProfileScreen.dart
@@ -47,35 +47,7 @@ class ProfileScreen extends StatelessWidget{
 
             Padding(
               padding: const EdgeInsets.all(8.0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    "Hike Length:",
-                    style: TextStyle(
-                        fontSize: 20.0,
-                        fontWeight: FontWeight.bold,
-                        fontFamily: 'Montserrat'),
-                  ),
-                  // Double ended slider here
-                  Text(
-                    "Elevation Gain:",
-                    style: TextStyle(
-                        fontSize: 20.0,
-                        fontWeight: FontWeight.bold,
-                        fontFamily: 'Montserrat'),
-                  ),
-                  Text(
-                    "Difficulty:",
-                    textAlign: TextAlign.start,
-                    style: TextStyle(
-                        fontSize: 20.0,
-                        fontWeight: FontWeight.bold,
-                        fontFamily: 'Montserrat'),
-                  ),
-                ],
-              ),
+              child: ProfileSelector(),
             ),
           ],
         ),
@@ -85,6 +57,137 @@ class ProfileScreen extends StatelessWidget{
     );
   }
 }
+
+class ProfileSelector extends StatefulWidget {
+  @override
+  _ProfileSelectorState createState() => _ProfileSelectorState();
+}
+
+class _ProfileSelectorState extends State<ProfileSelector> {
+  // Initialize selector sliders and boxes
+  RangeValues _currentElevationRange = const RangeValues(0, 50);
+  RangeValues _currentDistanceRange = const RangeValues(0, 50);
+  bool _easyChecked = true;
+  bool _moderateChecked = true;
+  bool _hardChecked = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          "Hike Length:",
+          style: TextStyle(
+              fontSize: 20.0,
+              fontWeight: FontWeight.bold,
+              fontFamily: 'Montserrat'),
+        ),
+        RangeSlider(
+          values: _currentDistanceRange,
+          min: 0,
+          max: 50,
+          divisions: 10,
+          labels: RangeLabels(
+            _currentDistanceRange.start.round().toString() + " km",
+            _currentDistanceRange.end.round().toString() + " km",
+          ),
+          onChanged: (RangeValues values) {
+            setState(() {
+              _currentDistanceRange = values;
+            });
+          },
+        ),
+        Text(
+          "Elevation Gain:",
+          style: TextStyle(
+              fontSize: 20.0,
+              fontWeight: FontWeight.bold,
+              fontFamily: 'Montserrat'),
+        ),
+        RangeSlider(
+          values: _currentElevationRange,
+          min: 0,
+          max: 50,
+          divisions: 10,
+          labels: RangeLabels(
+            _currentElevationRange.start.round().toString() + " km",
+            _currentElevationRange.end.round().toString() + " km",
+          ),
+          onChanged: (RangeValues values) {
+            setState(() {
+              _currentElevationRange = values;
+            });
+          },
+        ),
+        Text(
+          "Difficulty:",
+          textAlign: TextAlign.start,
+          style: TextStyle(
+              fontSize: 20.0,
+              fontWeight: FontWeight.bold,
+              fontFamily: 'Montserrat'),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Checkbox(
+              value: _easyChecked,
+              onChanged: (bool value) {
+                setState(() {
+                  _easyChecked = value;
+                });
+              },
+            ),
+            Text("Easy"),
+            SizedBox(width: 10,),
+            Checkbox(
+              value: _moderateChecked,
+              onChanged: (bool value) {
+                setState(() {
+                  _moderateChecked = value;
+                });
+              },
+            ),
+            Text("Moderate"),
+
+            // Special Checkbox
+            LabelledCheckbox(),
+          ],
+        ),
+      ],
+    );
+  }
+
+}
+
+class LabelledCheckbox extends StatefulWidget {
+  @override
+  _LabelledCheckboxState createState() => _LabelledCheckboxState();
+}
+
+class _LabelledCheckboxState extends State<LabelledCheckbox> {
+  bool _isChecked = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Checkbox(
+          value: _isChecked,
+          onChanged: (bool value) {
+            setState(() {
+              _isChecked = value;
+            });
+          },
+        ),
+        Text("Hard"),
+      ],
+    );
+  }
+}
+
 
 class getClipper extends CustomClipper<Path> {
   @override
