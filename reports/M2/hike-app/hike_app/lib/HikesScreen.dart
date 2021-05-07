acd3282510573389302af0ef2d7ef5aadcd43fd4
Jayden Wong <jaywong8288@gmail.com>
2021-05-07 16:19:22 -0700

    adds tap functionality to like and dislike buttons on hikes screen and updates background

--- reports/M2/hike-app/hike_app/lib/HikesScreen.dart
+++ reports/M2/hike-app/hike_app/lib/HikesScreen.dart
@@ -20,16 +20,6 @@ class HikesScreen extends StatefulWidget {
 
 
 class _HikesScreenState extends State<HikesScreen> with TickerProviderStateMixin{
-  // List<String> imagePaths = [
-  //   "assets/unmatched-hikes/alpha-mountain-east-ridge-route-1.jpg",
-  //   "assets/unmatched-hikes/alpha-mountain-east-ridge-route-2.jpg",
-  //   "assets/unmatched-hikes/bladu-pass-and-marriott-basin-trail-0.jpg",
-  //   "assets/unmatched-hikes/bladu-pass-and-marriott-basin-trail-1.jpg",
-  //   "assets/unmatched-hikes/bladu-pass-and-marriott-basin-trail-2.jpg",
-  //   "assets/unmatched-hikes/video-peak-0.jpg",
-  //   "assets/unmatched-hikes/video-peak-1.jpg",
-  //   "assets/unmatched-hikes/video-peak-2.jpg",
-  // ];
   Color backgroundColor = Colors.lightGreen[700];
   double likeOpacity = 0;
   double dislikeOpacity = 0;
@@ -52,175 +42,227 @@ class _HikesScreenState extends State<HikesScreen> with TickerProviderStateMixin
   @override
   Widget build(BuildContext context){
     CardController controller;
-    return Scaffold(
-      body: Stack(
-        children: [
-          Container(
-            height: MediaQuery.of(context).size.height,
-            width: MediaQuery.of(context).size.width,
-            color: backgroundColor,
-          ),
-          Center(
-            child: Icon(
-              Icons.terrain_rounded,
-              size: 200,
-              color: Colors.lightGreen[900],
-            ),
-          ),
-
-          Column(
-            mainAxisAlignment: MainAxisAlignment.end,
+      return Scaffold(
+          body: Stack(
             children: [
-              // Center(
-              //   child: Container(
-              //     height: 40,
-              //     width: 200,
-              //     child: Row(
-              //       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              //       children: [
-              //         Icon(
-              //           Icons.clear_rounded,
-              //           color: Colors.red,
-              //           size: 40,
-              //         ),
-              //         VerticalDivider(
-              //           color: Colors.grey,
-              //         ),
-              //
-              //         IconButton(
-              //           color: Colors.white,
-              //
-              //           icon: Icon(
-              //             Icons.check_rounded,
-              //             color: Colors.green,
-              //             size: 40,
-              //           ),
-              //           onPressed: (){
-              //             print("woo!");
-              //           },
-              //         ),
-              //       ],
-              //     ),
-              //   ),
-              // ),
-
-              ButtonBar(
-                alignment: MainAxisAlignment.center,
-                buttonHeight: 50,
-                buttonMinWidth: 90,
-                children: [
-                  RaisedButton(
-                    child: Icon(
-                      Icons.clear_rounded,
-                      color: Colors.red,
-                    ),
-                    onPressed: (){},
-                    color: Colors.white,
-                  ),
-                  RaisedButton(
-                    child: Icon(
-                      Icons.refresh_rounded,
-                      color: Colors.yellow[700],
-                    ),
-                    onPressed: (){},
-                    color: Colors.white,
-                  ),
-                  RaisedButton(
-                    child: Icon(
-                      Icons.check_rounded,
-                      color: Colors.green,
-                    ),
-                    onPressed: (){},
-                    color: Colors.white,
-                  ),
-                ],
+              Container(
+                height: MediaQuery
+                    .of(context)
+                    .size
+                    .height,
+                width: MediaQuery
+                    .of(context)
+                    .size
+                    .width,
+                color: backgroundColor,
               ),
-
-              SizedBox(height: 40,)
-            ],
-          ),
-          Center(
-            child: Container(
-              height: MediaQuery.of(context).size.height * 0.6,
-              child: TinderSwapCard(
-                orientation: AmassOrientation.BOTTOM,
-                totalNum: widget.unratedHikes.length,
-                stackNum: 3,
-                swipeEdge: 4.0,
-                maxWidth: MediaQuery.of(context).size.width * 0.9,
-                maxHeight: MediaQuery.of(context).size.width * 0.9,
-                minWidth: MediaQuery.of(context).size.width * 0.8,
-                minHeight: MediaQuery.of(context).size.width * 0.8,
-                cardBuilder: (context, index) => Card(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    crossAxisAlignment: CrossAxisAlignment.start,
+              Center(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      Padding(
-                        padding: const EdgeInsets.fromLTRB(0,20,0,0),
-                        child: Center(child: Image.network(widget.unratedHikes[index].img_1)),
-                      ),
-                      Expanded(
-                        flex: 1,
-                        child: Center(
-                          child: Container(
-                            child: Text(
-                                widget.unratedHikes[index].parsedName(),
-                              style: TextStyle(
-                                fontSize: 20,
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            "Refresh, or come back later!",
+                            style: TextStyle(
+                                fontSize: 25,
+                                fontFamily: 'Montserat',
                                 fontWeight: FontWeight.bold,
-                              ),
-                            )
+                                color: Colors.grey[700]
+                            ),
                           ),
-                        ),
+                          Icon(
+                            Icons.terrain_rounded,
+                            size: 200,
+                            color: Colors.lightGreen[900],
+                          ),
+                        ],
                       )
                     ],
+                  )
+
+              ),
+
+              Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  // Center(
+                  //   child: Container(
+                  //     height: 40,
+                  //     width: 200,
+                  //     child: Row(
+                  //       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  //       children: [
+                  //         Icon(
+                  //           Icons.clear_rounded,
+                  //           color: Colors.red,
+                  //           size: 40,
+                  //         ),
+                  //         VerticalDivider(
+                  //           color: Colors.grey,
+                  //         ),
+                  //
+                  //         IconButton(
+                  //           color: Colors.white,
+                  //
+                  //           icon: Icon(
+                  //             Icons.check_rounded,
+                  //             color: Colors.green,
+                  //             size: 40,
+                  //           ),
+                  //           onPressed: (){
+                  //             print("woo!");
+                  //           },
+                  //         ),
+                  //       ],
+                  //     ),
+                  //   ),
+                  // ),
+
+                  ButtonBar(
+                    alignment: MainAxisAlignment.center,
+                    buttonHeight: 50,
+                    buttonMinWidth: 90,
+                    children: [
+                      RaisedButton(
+                        child: Icon(
+                          Icons.clear_rounded,
+                          color: Colors.red,
+                        ),
+                        onPressed: () {
+                          controller.triggerLeft();
+                        },
+                        color: Colors.white,
+                      ),
+                      RaisedButton(
+                        child: Icon(
+                          Icons.refresh_rounded,
+                          color: Colors.yellow[700],
+                        ),
+                        onPressed: () {
+                          //TODO: Add hike refresh functionality.
+                        },
+                        color: Colors.white,
+                      ),
+                      RaisedButton(
+                        child: Icon(
+                          Icons.check_rounded,
+                          color: Colors.green,
+                        ),
+                        onPressed: () {
+                          controller.triggerRight();
+                        },
+                        color: Colors.white,
+                      ),
+                    ],
                   ),
-                ),
-                cardController: controller = CardController(),
-                swipeUpdateCallback:
-                    (DragUpdateDetails details, Alignment align) {
-                  /// Get swiping card's alignment
-                  if (align.x < 0) {
-                    //Card is LEFT swiping
-                    setState(() {
-                      backgroundColor = Colors.red;
-                    });
-                  } else if (align.x > 0) {
-                    //Card is RIGHT swiping
-                    setState(() {
-                      backgroundColor = Colors.blue;
-                    });
-                  }
-                },
-                swipeCompleteCallback:
-                    (CardSwipeOrientation orientation, int index) {
-                    /// Get orientation & index of swiped card!
-                    if (orientation == CardSwipeOrientation.RIGHT) {
-                      // If swiped right, mark as liked and add to matches
-                      widget.unratedHikes[index].rating = 'liked';
-                      widget.matchedHikes.add(widget.unratedHikes[index]);
-                    } else {
-                      // Item was swiped left, mark as disliked
-                      widget.unratedHikes[index].rating = 'disliked';
-                    }
 
-                    if (orientation != CardSwipeOrientation.RECOVER) {
-                      // If card was swiped, move the swiped card to the rated list
-                      widget.ratedHikes.add(widget.unratedHikes[index]);
-                      widget.unratedHikes.remove(widget.unratedHikes[index]);
-                    }
+                  SizedBox(height: 40,)
+                ],
+              ),
+              Center(
+                  child: Container(
+                      height: MediaQuery
+                          .of(context)
+                          .size
+                          .height * 0.6,
+                      child: TinderSwapCard(
+                        orientation: AmassOrientation.BOTTOM,
+                        totalNum: widget.unratedHikes.length,
+                        stackNum: 3,
+                        swipeEdge: 4.0,
+                        maxWidth: MediaQuery
+                            .of(context)
+                            .size
+                            .width * 0.9,
+                        maxHeight: MediaQuery
+                            .of(context)
+                            .size
+                            .width * 0.9,
+                        minWidth: MediaQuery
+                            .of(context)
+                            .size
+                            .width * 0.8,
+                        minHeight: MediaQuery
+                            .of(context)
+                            .size
+                            .width * 0.8,
+                        cardBuilder: (context, index) =>
+                            Card(
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment
+                                    .spaceEvenly,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Padding(
+                                    padding: const EdgeInsets.fromLTRB(
+                                        0, 20, 0, 0),
+                                    child: Center(child: Image.network(
+                                        widget.unratedHikes[index].img_1)),
+                                  ),
+                                  Expanded(
+                                    flex: 1,
+                                    child: Center(
+                                      child: Container(
+                                          child: Text(
+                                            widget.unratedHikes[index]
+                                                .parsedName(),
+                                            style: TextStyle(
+                                              fontSize: 20,
+                                              fontWeight: FontWeight.bold,
+                                            ),
+                                          )
+                                      ),
+                                    ),
+                                  )
+                                ],
+                              ),
+                            ),
+                        cardController: controller = CardController(),
+                        swipeUpdateCallback:
+                            (DragUpdateDetails details, Alignment align) {
+                          /// Get swiping card's alignment
+                          if (align.x < 0) {
+                            //Card is LEFT swiping
+                            setState(() {
+                              backgroundColor = Colors.red;
+                            });
+                          } else if (align.x > 0) {
+                            //Card is RIGHT swiping
+                            setState(() {
+                              backgroundColor = Colors.blue;
+                            });
+                          }
+                        },
+                        swipeCompleteCallback:
+                            (CardSwipeOrientation orientation, int index) {
+                          /// Get orientation & index of swiped card!
+                          if (orientation == CardSwipeOrientation.RIGHT) {
+                            // If swiped right, mark as liked and add to matches
+                            widget.unratedHikes[index].rating = 'liked';
+                            widget.matchedHikes.add(widget.unratedHikes[index]);
+                          } else {
+                            // Item was swiped left, mark as disliked
+                            widget.unratedHikes[index].rating = 'disliked';
+                          }
 
-                    setState(() {
-                      backgroundColor = Colors.lightGreen[700];
-                    });
+                          if (orientation != CardSwipeOrientation.RECOVER) {
+                            // If card was swiped, move the swiped card to the rated list
+                            widget.ratedHikes.add(widget.unratedHikes[index]);
+                            widget.unratedHikes.remove(
+                                widget.unratedHikes[index]);
+                          }
 
-                },
-              )
-            )
-          ),
-        ],
-      )
-    );
+                          setState(() {
+                            backgroundColor = Colors.lightGreen[700];
+                          });
+                        },
+                      )
+                  )
+              ),
+            ],
+          )
+      );
   }
 }
